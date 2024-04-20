e3be15805c464a32c66a2b331223a2b8d74393bc
claud <crobinson@tec.mx>
2024-04-20 14:05:12 -0600

    Initial Project

---
+++ src/main/java/my/edu/MyRecipe2024/IndexController.java
@@ -0,0 +1,13 @@
+package my.edu.MyRecipe2024;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index", "index.html"})
+    public String indexPage(){
+        return "index";
+    }
+}
