3125e0a78b51b0b99291089a576bd3bdd6ab7459
claud <crobinson@tec.mx>
2024-04-20 19:11:08 -0600

    Unit of Measure

--- src/main/java/my/edu/MyRecipe2024/controllers/IndexController.java
+++ src/main/java/my/edu/MyRecipe2024/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package my.edu.MyRecipe2024;
+package my.edu.MyRecipe2024.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
